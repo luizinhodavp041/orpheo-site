6b198931a8e9e5b8bcf5ecbfd3e71032e47965ef
luizinhodavp041 <luizinhodavp041@gmail.com>
2024-12-11 23:57:35 -0300

    sem eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // isso ignora o ESLint durante o build
+  },
 };
 
 export default nextConfig;
